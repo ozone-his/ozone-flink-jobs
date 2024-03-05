ff063b8fc7685b848e02c89647224c4fde75c996
Emmanuel Nyachoke <achachiez@gmail.com>
2024-03-05 09:48:40 +0300

    OZ-485: Allow setting of process and network memory and increase defaults

--- src/main/java/com/ozonehis/data/pipelines/utils/Environment.java
+++ src/main/java/com/ozonehis/data/pipelines/utils/Environment.java
@@ -7,6 +7,7 @@ import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 import java.util.stream.Stream;
 import org.apache.flink.configuration.Configuration;
+import org.apache.flink.configuration.JobManagerOptions;
 import org.apache.flink.configuration.MemorySize;
 import org.apache.flink.configuration.TaskManagerOptions;
 import org.apache.flink.runtime.client.JobStatusMessage;
@@ -33,9 +34,11 @@ public class Environment {
 
     public static MiniCluster initMiniClusterWithEnv(Boolean isStreaming) throws Exception {
         Configuration flinkConfig = new Configuration();
-        flinkConfig.set(TaskManagerOptions.NETWORK_MEMORY_MIN, MemorySize.parse("64m"));
-        flinkConfig.set(TaskManagerOptions.NETWORK_MEMORY_MAX, MemorySize.parse("64m"));
-
+        flinkConfig.set(TaskManagerOptions.NETWORK_MEMORY_MIN, MemorySize.parse(System.getenv().getOrDefault("NETWORK_MEMORY_MIN", "500m")));
+        flinkConfig.set(TaskManagerOptions.NETWORK_MEMORY_MAX, MemorySize.parse(System.getenv().getOrDefault("NETWORK_MEMORY_MAX", "500m")));
+        flinkConfig.set(TaskManagerOptions.MANAGED_MEMORY_FRACTION, Float.parseFloat(System.getenv().getOrDefault("MANAGED_MEMORY_FRACTION", "0.7")));
+        flinkConfig.set(TaskManagerOptions.TOTAL_PROCESS_MEMORY, MemorySize.parse(System.getenv().getOrDefault("TOTAL_PROCESS_MEMORY", "1g")));
+        flinkConfig.set(JobManagerOptions.TOTAL_PROCESS_MEMORY, MemorySize.parse(System.getenv().getOrDefault("TOTAL_PROCESS_MEMORY", "1g")));
         flinkConfig.setString("restart-strategy.type", "exponential-delay");
         flinkConfig.setString("execution.checkpointing.mode", "EXACTLY_ONCE");
         flinkConfig.setString("execution.checkpointing.interval", "10min");
