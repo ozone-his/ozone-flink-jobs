b4113fbaa24b6aa112d05428e5032ecbc9953d5d
wluyima <wyclif@openmrs.org>
2024-04-09 16:36:48 -0500

    OZ-68: Restructure and consolidated duplicated code into a base class

---
+++ src/main/java/com/ozonehis/data/pipelines/Bootstrap.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright (C) Amiyul LLC - All Rights Reserved
+ *
+ * This source code is protected under international copyright law. All rights
+ * reserved and protected by the copyright holder.
+ *
+ * This file is confidential and only available to authorized individuals with the
+ * permission of the copyright holder. If you encounter this file and do not have
+ * permission, please contact the copyright holder and delete this file.
+ */
+package com.ozonehis.data.pipelines;
+
+import com.ozonehis.data.pipelines.batch.BatchJob;
+import com.ozonehis.data.pipelines.streaming.StreamJob;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Contains the logic to start and execute an ETL job.
+ */
+public class Bootstrap {
+
+    private static final Logger LOG = LoggerFactory.getLogger(Bootstrap.class);
+
+    public static void main(String[] args) {
+        BaseJob job;
+        if (args.length == 0) {
+            LOG.info("Executing flink job in stream mode");
+            job = new StreamJob();
+        } else if ("batch".equalsIgnoreCase(args[0])) {
+            LOG.info("Executing flink job in batch mode");
+            job = new BatchJob();
+        } else {
+            throw new RuntimeException("Unsupported commandline argument: " + args[0]);
+        }
+
+        job.execute();
+    }
+}
---
+++ src/main/java/com/ozonehis/data/pipelines/batch/BatchJob.java
@@ -0,0 +1,69 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.ozonehis.data.pipelines.batch;
+
+import com.ozonehis.data.pipelines.BaseJob;
+import com.ozonehis.data.pipelines.config.JdbcSourceConfig;
+import com.ozonehis.data.pipelines.utils.CommonUtils;
+import com.ozonehis.data.pipelines.utils.ConnectorUtils;
+import com.ozonehis.data.pipelines.utils.QueryFile;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * <p>
+ * For a tutorial how to write a Flink streaming application, check the tutorials and examples on
+ * the <a href="https://flink.apache.org/docs/stable/">Flink Website</a>.
+ * <p>
+ * To package your application into a JAR file for execution, run 'mvn clean package' on the command
+ * line.
+ * <p>
+ * If you change the name of the main class (with the public static void main(String[] args))
+ * method, change the respective entry in the POM.xml file (simply search for 'mainClass').
+ */
+public class BatchJob extends BaseJob {
+
+    private static final Logger LOG = LoggerFactory.getLogger(BatchJob.class);
+
+    @Override
+    public void registerSources() {
+        LOG.info("Registering database tables for batch processing");
+        for (JdbcSourceConfig jdbcSourceConfig :
+                CommonUtils.getConfig(configFilePath).getJdbcSources()) {
+            Stream<QueryFile> tables = CommonUtils.getSQL(jdbcSourceConfig.getTableDefinitionsPath()).stream();
+            tables.forEach(s -> {
+                Map<String, String> connectorOptions = Stream.of(new String[][] {
+                            {"connector", "jdbc"},
+                            {"url", jdbcSourceConfig.getDatabaseUrl()},
+                            {"username", jdbcSourceConfig.getUsername()},
+                            {"password", jdbcSourceConfig.getPassword()},
+                            {"table-name", s.fileName},
+                        })
+                        .collect(Collectors.toMap(data -> data[0], data -> data[1]));
+
+                String queryDSL = s.content + "\n" + " WITH (\n"
+                        + ConnectorUtils.propertyJoiner(",", "=").apply(connectorOptions) + ")";
+                tableEnv.executeSql(queryDSL);
+            });
+        }
+    }
+}
