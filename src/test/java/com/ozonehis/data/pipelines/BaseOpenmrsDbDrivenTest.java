827f0d6505e4fa70a59c80639638589ad9abafb4
wluyima <wyclif@openmrs.org>
2024-04-11 18:35:27 -0500

    OZ-68: Added test logic to create OpenMRS database

--- src/test/java/com/ozonehis/data/pipelines/BaseOpenmrsDbDrivenTest.java
+++ src/test/java/com/ozonehis/data/pipelines/BaseOpenmrsDbDrivenTest.java
@@ -15,49 +15,83 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.stream.Stream;
-
+import liquibase.Liquibase;
+import liquibase.database.Database;
+import liquibase.database.DatabaseFactory;
+import liquibase.database.jvm.JdbcConnection;
+import liquibase.exception.LiquibaseException;
+import liquibase.resource.ClassLoaderResourceAccessor;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.testcontainers.lifecycle.Startables;
 
 public abstract class BaseOpenmrsDbDrivenTest {
-	
-	private static final MySQLTestDatabase OPENMRS_DB = new MySQLTestDatabase();
-	
-	private static final PostgresTestDatabase ANALYTICS_DB = new PostgresTestDatabase();
-	
-	protected Connection getOpenmrsDbConnection() {
-		try {
-			return DriverManager.getConnection(OPENMRS_DB.getJdbcUrl(), OPENMRS_DB.USER, OPENMRS_DB.PASSWORD);
-		}
-		catch (SQLException e) {
-			throw new RuntimeException(e);
-		}
-	}
-	
-	protected Connection getAnalyticsDbConnection() {
-		try {
-			return DriverManager.getConnection(ANALYTICS_DB.getJdbcUrl(), ANALYTICS_DB.USER, ANALYTICS_DB.PASSWORD);
-		}
-		catch (SQLException e) {
-			throw new RuntimeException(e);
-		}
-	}
-	
-	@BeforeAll
-	public static void beforeAll() {
-		OPENMRS_DB.start(false);
-		ANALYTICS_DB.start(false);
-		Startables.deepStart(Stream.of(OPENMRS_DB.getDbContainer(), ANALYTICS_DB.getDbContainer())).join();
-	}
-	
-	@AfterAll
-	public static void afterAll() {
-		OPENMRS_DB.shutdown();
-		ANALYTICS_DB.shutdown();
-	}
-	
-	protected void addDataToSource(String file) throws Exception {
-		TestUtils.executeScript(file, getOpenmrsDbConnection());
-	}
+
+    private static final String ROOT = "org/openmrs/liquibase/";
+
+    private static final String LIQUIBASE_SCHEMA_2_5 = ROOT + "snapshots/schema-only/liquibase-schema-only-2.5.x.xml";
+
+    private static final String LIQUIBASE_UPDATE_2_6 = ROOT + "updates/liquibase-update-to-latest-2.6.x.xml";
+
+    private static final MySQLTestDatabase OPENMRS_DB = new MySQLTestDatabase();
+
+    private static final PostgresTestDatabase ANALYTICS_DB = new PostgresTestDatabase();
+
+    private static Connection connectToOpenmrsDbConn() {
+        try {
+            return DriverManager.getConnection(OPENMRS_DB.getJdbcUrl(), OPENMRS_DB.USER, OPENMRS_DB.PASSWORD);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    protected Connection getOpenmrsDbConnection() {
+        return connectToOpenmrsDbConn();
+    }
+
+    protected Connection getAnalyticsDbConnection() {
+        try {
+            return DriverManager.getConnection(ANALYTICS_DB.getJdbcUrl(), ANALYTICS_DB.USER, ANALYTICS_DB.PASSWORD);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @BeforeAll
+    public static void beforeAll() {
+        OPENMRS_DB.start(false);
+        ANALYTICS_DB.start(false);
+        Startables.deepStart(Stream.of(OPENMRS_DB.getDbContainer(), ANALYTICS_DB.getDbContainer()))
+                .join();
+        createOpenmrsSchema();
+    }
+
+    @AfterAll
+    public static void afterAll() {
+        OPENMRS_DB.shutdown();
+        ANALYTICS_DB.shutdown();
+    }
+
+    private static void createOpenmrsSchema() {
+        try {
+            updateDatabase(getLiquibase(LIQUIBASE_SCHEMA_2_5, connectToOpenmrsDbConn()));
+            updateDatabase(getLiquibase(LIQUIBASE_UPDATE_2_6, connectToOpenmrsDbConn()));
+        } catch (LiquibaseException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static Liquibase getLiquibase(String file, Connection connection) throws LiquibaseException {
+        Database db = DatabaseFactory.getInstance().findCorrectDatabaseImplementation(new JdbcConnection(connection));
+        return new Liquibase(file, new ClassLoaderResourceAccessor(BaseOpenmrsDbDrivenTest.class.getClassLoader()), db);
+    }
+
+    private static void updateDatabase(Liquibase liquibase) throws LiquibaseException {
+        liquibase.update((String) null);
+        liquibase.getDatabase().getConnection().commit();
+    }
+
+    protected void addDataToOpenmrs(String file) throws Exception {
+        TestUtils.executeScript(file, getOpenmrsDbConnection());
+    }
 }
