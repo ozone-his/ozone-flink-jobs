2a0846f449c203a179a7617fb8359661cd9f7e47
wluyima <wyclif@openmrs.org>
2024-04-11 18:35:27 -0500

    OZ-68: Added test logic to create OpenMRS database

--- src/test/java/com/ozonehis/data/pipelines/TestUtils.java
+++ src/test/java/com/ozonehis/data/pipelines/TestUtils.java
@@ -13,24 +13,23 @@ package com.ozonehis.data.pipelines;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.sql.Connection;
-
 import org.apache.ibatis.jdbc.ScriptRunner;
 
 public class TestUtils {
-	
-	public static void executeScript(String filename, Connection connection) {
-		final String path = TestUtils.class.getClassLoader().getResource(filename).getPath();
-		FileReader fileReader;
-		try {
-			fileReader = new FileReader(path);
-		}
-		catch (FileNotFoundException e) {
-			throw new RuntimeException(e);
-		}
-		
-		ScriptRunner scriptRunner = new ScriptRunner(connection);
-		scriptRunner.setSendFullScript(false);
-		scriptRunner.setStopOnError(true);
-		scriptRunner.runScript(fileReader);
-	}
+
+    public static void executeScript(String filename, Connection connection) {
+        final String path =
+                TestUtils.class.getClassLoader().getResource(filename).getPath();
+        FileReader fileReader;
+        try {
+            fileReader = new FileReader(path);
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+
+        ScriptRunner scriptRunner = new ScriptRunner(connection);
+        scriptRunner.setSendFullScript(false);
+        scriptRunner.setStopOnError(true);
+        scriptRunner.runScript(fileReader);
+    }
 }
