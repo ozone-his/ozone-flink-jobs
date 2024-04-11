ad0bec0977fbb0d3857b6be364f9fe44e6549055
wluyima <wyclif@openmrs.org>
2024-04-11 16:37:59 -0500

    OZ-68: Implemented mechanism to load test data into a TestDatabase

--- src/test/java/com/ozonehis/data/pipelines/BaseOpenmrsDbDrivenTest.java
+++ src/test/java/com/ozonehis/data/pipelines/BaseOpenmrsDbDrivenTest.java
@@ -11,35 +11,53 @@
  */
 package com.ozonehis.data.pipelines;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.stream.Stream;
+
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.testcontainers.lifecycle.Startables;
 
 public abstract class BaseOpenmrsDbDrivenTest {
-
-    private static final MySQLTestDatabase OPENMRS_DB = new MySQLTestDatabase();
-
-    private static final PostgresTestDatabase ANALYTICS_DB = new PostgresTestDatabase();
-
-    protected String getOpenmrsJdbcUrl() {
-        return OPENMRS_DB.getJdbcUrl();
-    }
-
-    protected String getAnalyticsJdbcUrl() {
-        return ANALYTICS_DB.getJdbcUrl();
-    }
-
-    @BeforeAll
-    public static void beforeAll() {
-        OPENMRS_DB.start(false);
-        ANALYTICS_DB.start(false);
-        Startables.deepStart(Stream.of(OPENMRS_DB.getDbContainer(), ANALYTICS_DB.getDbContainer()))
-                .join();
-    }
-
-    @AfterAll
-    public static void afterAll() {
-        OPENMRS_DB.shutdown();
-    }
+	
+	private static final MySQLTestDatabase OPENMRS_DB = new MySQLTestDatabase();
+	
+	private static final PostgresTestDatabase ANALYTICS_DB = new PostgresTestDatabase();
+	
+	protected Connection getOpenmrsDbConnection() {
+		try {
+			return DriverManager.getConnection(OPENMRS_DB.getJdbcUrl(), OPENMRS_DB.USER, OPENMRS_DB.PASSWORD);
+		}
+		catch (SQLException e) {
+			throw new RuntimeException(e);
+		}
+	}
+	
+	protected Connection getAnalyticsDbConnection() {
+		try {
+			return DriverManager.getConnection(ANALYTICS_DB.getJdbcUrl(), ANALYTICS_DB.USER, ANALYTICS_DB.PASSWORD);
+		}
+		catch (SQLException e) {
+			throw new RuntimeException(e);
+		}
+	}
+	
+	@BeforeAll
+	public static void beforeAll() {
+		OPENMRS_DB.start(false);
+		ANALYTICS_DB.start(false);
+		Startables.deepStart(Stream.of(OPENMRS_DB.getDbContainer(), ANALYTICS_DB.getDbContainer())).join();
+	}
+	
+	@AfterAll
+	public static void afterAll() {
+		OPENMRS_DB.shutdown();
+		ANALYTICS_DB.shutdown();
+	}
+	
+	protected void addDataToSource(String file) throws Exception {
+		TestUtils.executeScript(file, getOpenmrsDbConnection());
+	}
 }
