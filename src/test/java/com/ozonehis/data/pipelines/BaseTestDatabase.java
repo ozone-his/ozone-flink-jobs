dc2acc5bb5b5446524d25a5919e53d4466b061ce
wluyima <wyclif@openmrs.org>
2024-04-11 16:37:59 -0500

    OZ-68: Implemented mechanism to load test data into a TestDatabase

--- src/test/java/com/ozonehis/data/pipelines/BaseTestDatabase.java
+++ src/test/java/com/ozonehis/data/pipelines/BaseTestDatabase.java
@@ -12,33 +12,34 @@
 package com.ozonehis.data.pipelines;
 
 import java.util.stream.Stream;
+
 import org.testcontainers.containers.JdbcDatabaseContainer;
 import org.testcontainers.lifecycle.Startables;
 
 public abstract class BaseTestDatabase {
-
-    public static final String TEST_USER = "test-user";
-
-    public static final String TEST_PASSWORD = "test-pass";
-
-    public static final String TEST_DB_NAME = "test-db";
-
-    public void start(boolean wait) {
-        getDbContainer().withDatabaseName(TEST_DB_NAME);
-        getDbContainer().withUsername(TEST_USER);
-        getDbContainer().withPassword(TEST_PASSWORD);
-        if (wait) {
-            Startables.deepStart(Stream.of(getDbContainer())).join();
-        }
-    }
-
-    public String getJdbcUrl() {
-        return getDbContainer().getJdbcUrl();
-    }
-
-    public void shutdown() {
-        getDbContainer().stop();
-    }
-
-    public abstract JdbcDatabaseContainer<?> getDbContainer();
+	
+	public static final String USER = "test-user";
+	
+	public static final String PASSWORD = "test-password";
+	
+	public static final String DB_NAME = "test-db";
+	
+	public void start(boolean wait) {
+		getDbContainer().withDatabaseName(DB_NAME);
+		getDbContainer().withUsername(USER);
+		getDbContainer().withPassword(PASSWORD);
+		if (wait) {
+			Startables.deepStart(Stream.of(getDbContainer())).join();
+		}
+	}
+	
+	public String getJdbcUrl() {
+		return getDbContainer().getJdbcUrl();
+	}
+	
+	public void shutdown() {
+		getDbContainer().stop();
+	}
+	
+	public abstract JdbcDatabaseContainer<?> getDbContainer();
 }
---
+++ src/test/java/com/ozonehis/data/pipelines/TestUtils.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright (C) Amiyul LLC - All Rights Reserved
+ *
+ * This source code is protected under international copyright law. All rights
+ * reserved and protected by the copyright holder.
+ *
+ * This file is confidential and only available to authorized individuals with the
+ * permission of the copyright holder. If you encounter this file and do not have
+ * permission, please contact the copyright holder and delete this file.
+ */
+package com.ozonehis.data.pipelines;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.sql.Connection;
+
+import org.apache.ibatis.jdbc.ScriptRunner;
+
+public class TestUtils {
+	
+	public static void executeScript(String filename, Connection connection) {
+		final String path = TestUtils.class.getClassLoader().getResource(filename).getPath();
+		FileReader fileReader;
+		try {
+			fileReader = new FileReader(path);
+		}
+		catch (FileNotFoundException e) {
+			throw new RuntimeException(e);
+		}
+		
+		ScriptRunner scriptRunner = new ScriptRunner(connection);
+		scriptRunner.setSendFullScript(false);
+		scriptRunner.setStopOnError(true);
+		scriptRunner.runScript(fileReader);
+	}
+}
