09ecd94a13df7863005f70cc685235f26dba0160
wluyima <wyclif@openmrs.org>
2024-04-12 17:36:26 -0500

    OZ-68: Added initial implementation of PatientBatchJobTest

--- src/test/java/com/ozonehis/data/pipelines/TestUtils.java
+++ src/test/java/com/ozonehis/data/pipelines/TestUtils.java
@@ -11,6 +11,13 @@
 package com.ozonehis.data.pipelines;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.jdbc.datasource.init.ScriptUtils;
 
@@ -18,5 +25,23 @@ public class TestUtils {
 
     public static void executeScript(String file, Connection connection) {
         ScriptUtils.executeSqlScript(connection, new ClassPathResource(file));
+    }
+
+    public static List<Map<String, Object>> getRows(String table, Connection connection) {
+        List<Map<String, Object>> rows = new ArrayList<>();
+        try (Statement s = connection.createStatement();
+                ResultSet rs = s.executeQuery("SELECT * FROM " + table)) {
+            while (rs.next()) {
+                Map<String, Object> row = new HashMap<>();
+                for (int i = 1; i < rs.getMetaData().getColumnCount() + 1; i++) {
+                    row.put(rs.getMetaData().getColumnName(i), rs.getObject(i));
+                }
+            }
+
+            rs.close();
+            return rows;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
