79f4d6af24f15d91ae915cd63989a902027e3e4f
wluyima <wyclif@openmrs.org>
2024-04-15 15:06:17 -0500

    OZ-68: Added catalog config to tests and configured surefire plugin

--- src/main/java/com/ozonehis/data/pipelines/Constants.java
+++ src/main/java/com/ozonehis/data/pipelines/Constants.java
@@ -12,7 +12,7 @@ package com.ozonehis.data.pipelines;
 
 public class Constants {
 
-    public static final String ENV_ANALYTICS_CONFIG_FILE_PATH = "ANALYTICS_CONFIG_FILE_PATH";
+    public static final String PROP_ANALYTICS_CONFIG_FILE_PATH = "ANALYTICS_CONFIG_FILE_PATH";
 
     public static final String DEFAULT_ANALYTICS_CONFIG_FILE_PATH = "/etc/analytics/config.yaml";
 }
