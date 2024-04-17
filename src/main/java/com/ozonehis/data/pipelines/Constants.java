582b119b01fb5be6de840522221d500f82b396d9
wluyima <wyclif@openmrs.org>
2024-04-17 16:03:45 -0500

    OZ-68: Made REST PORT configurable and applied more fixes to PatientBatchJobTest

--- src/main/java/com/ozonehis/data/pipelines/Constants.java
+++ src/main/java/com/ozonehis/data/pipelines/Constants.java
@@ -15,4 +15,6 @@ public class Constants {
     public static final String PROP_ANALYTICS_CONFIG_FILE_PATH = "ANALYTICS_CONFIG_FILE_PATH";
 
     public static final String DEFAULT_ANALYTICS_CONFIG_FILE_PATH = "/etc/analytics/config.yaml";
+
+    public static final String PROP_FLINK_REST_PORT = "FLINK_REST_PORT";
 }
