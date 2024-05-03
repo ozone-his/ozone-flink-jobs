ab1f5baa2b428ac4c8d4c2dc41e73e2e6ee75cee
wluyima <wyclif@openmrs.org>
2024-05-03 23:55:57 +0300

    OZ-68: Updated test framework to create external docker network and fixed main class names in pom file

--- src/test/java/com/ozonehis/data/pipelines/TestUtils.java
+++ src/test/java/com/ozonehis/data/pipelines/TestUtils.java
@@ -1,6 +1,10 @@
 package com.ozonehis.data.pipelines;
 
+import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.sql.Connection;
 import java.sql.ResultSet;
@@ -10,6 +14,9 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.function.Consumer;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.jdbc.datasource.init.ScriptUtils;
 
@@ -67,5 +74,52 @@ public class TestUtils {
 
         // Really! No port is available?
         throw new RuntimeException("No available port found");
+    }
+
+    public static void createNetworkIfNecessary(String name, String workingDir) {
+        try {
+            execute("docker network inspect " + name, workingDir);
+        } catch (RuntimeException t) {
+            // Ignore, network does not exist
+            execute("docker network create -d bridge " + name, workingDir);
+        }
+    }
+
+    private static void execute(String cmd, String workingDir) {
+        ProcessBuilder builder = new ProcessBuilder();
+        builder.command("sh", "-c", cmd).directory(new File(workingDir));
+        ExecutorService executor = Executors.newFixedThreadPool(2);
+        try {
+            Process process = builder.start();
+            OutputCaptor outputCaptor = new OutputCaptor(process.getInputStream(), System.out::println);
+            OutputCaptor errorCaptor = new OutputCaptor(process.getErrorStream(), System.err::println);
+            executor.execute(outputCaptor);
+            executor.execute(errorCaptor);
+            int exitCode = process.waitFor();
+            if (exitCode != 0) {
+                throw new RuntimeException("An error occurred while executing: " + cmd + ", exit code: " + exitCode);
+            }
+        } catch (Throwable t) {
+            throw new RuntimeException(t);
+        } finally {
+            executor.shutdownNow();
+        }
+    }
+
+    private static class OutputCaptor implements Runnable {
+
+        private InputStream inputStream;
+
+        private Consumer<String> consumer;
+
+        public OutputCaptor(InputStream inputStream, Consumer<String> consumer) {
+            this.inputStream = inputStream;
+            this.consumer = consumer;
+        }
+
+        @Override
+        public void run() {
+            new BufferedReader(new InputStreamReader(inputStream)).lines().forEach(consumer);
+        }
     }
 }
